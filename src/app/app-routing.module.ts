8133cf3e54441e17d424033b8fa37f774985378d
Xinh Nguyen <nguyentrucxjnh@gmail.com>
2020-08-25 17:15:45 +0700

    :star: Add home module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [{ path: 'news', loadChildren: () => import('./news/news.module').then(m => m.NewsModule) }];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'news',
+    loadChildren: () => import('./news/news.module').then(m => m.NewsModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
